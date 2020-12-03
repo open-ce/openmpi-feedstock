d79e907d87cf414ea4cc60ec39c0c6fc712f3d78
Bryant Nelson <bryant.nelson@ibm.com>
2020-12-03 13:16:41 -0600

    Fix activate.sh (#11)

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -2,11 +2,11 @@
 
 if [ ! -z "$OPAL_PREFIX" ]
 then
-    export OPEN_MPI_OPAL_PREFIX_OLD =$OPAL_PREFIX
+    export OPEN_MPI_OPAL_PREFIX_OLD="$OPAL_PREFIX"
 fi
 if [ ! -z "$MPI_ROOT" ]
 then
-    export OPEN_MPI_MPI_ROOT_OLD=$MPI_ROOT
+    export OPEN_MPI_MPI_ROOT_OLD="$MPI_ROOT"
 fi
-export OPAL_PREFIX=$CONDA_PREFIX
-export MPI_ROOT=$CONDA_PREFIX
+export OPAL_PREFIX="$CONDA_PREFIX"
+export MPI_ROOT="$CONDA_PREFIX"
